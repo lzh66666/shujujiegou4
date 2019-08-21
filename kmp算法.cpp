d87c6a0b4e5dc5e581209ed16c675cf6984f702b
lzh <luzehua@zhongbei.com>
2019-08-21 12:21:03 +0800

    KMP算法(改进版)

--- kmp算法.cpp
+++ kmp算法.cpp
@@ -1,124 +1,129 @@
 #include <iostream>
+#include <string.h> 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define OK 1
 #define ERROR 0
-#define TURE 1
-#define FALSE 0 
+#define OK 1
+#define MAXSTRLEN 255//串的初始长度 
 
-#define MAXSTRLEN 255
-
-typedef unsigned char SString[MAXSTRLEN + 1];
-
+typedef char SString[MAXSTRLEN+1];//0号单元存放串的长度 
 typedef int Status;
 
-Status StrAssign(SString &T,char *chars);  //生成串  
-Status Print(SString T);  //输出串 
-int index_KMP(SString S,SString T,int pos);
-void get_next(SString T,int *next);
+void get_next(SString T,Status next[]);
+Status StrAssign(SString T,char *chars);
+void StrPrint(SString T);
 
-int main(int argc, char** argv) {
+int Index_KMP(SString S,SString T,Status pos);//改进版 
+void get_nextval(SString T,Status nextval[]);//改进next数组函数版 
+
+
+int main(int argc, char** argv) 
+{
+	char s[256],t[256];
+	Status pos;
 	SString S,T;
-	int pos,i;
-	char chars[1000],chars1[1000];
 	
-	printf("请输入串S:\n");
-	gets(chars1);
-	StrAssign(S,chars1);
-	Print(S);
+	printf("请输入字符串S：\n");
+	gets(s);
+	StrAssign(S,s);
+//	printf("输出字符串S：\n");
+//	StrPrint(S); 
 	
-	printf("请输入串T:\n");
-	gets(chars);
-	StrAssign(T,chars);
-	Print(T);
+	printf("请输入字符串T：\n"); 
+	gets(t);
+	StrAssign(T,t);
+//	printf("输出字符串T：\n");
+//	StrPrint(T);
 	
-	printf("请输入要重第几位开始查找:\n");
+	printf("请输入位置(第pos个字符之后):\n");
 	scanf("%d",&pos);
-	i=index_KMP(S,T,pos);
-	printf("在第%d位",i);
+	printf("模式串T在主串S的位置为%d\n",Index_KMP(S,T,pos));
 	
-	/*printf("请输入串T:\n");
-	gets(chars);
-	StrAssign(T,chars);
-	Print(T);
+/*	printf("请输入串T:\n");
+	gets(s);
+	StrAssign(T,s);
 	
 	int next[100];
 //	ababaaababaa
-	get_next(T,next);
-	for(int i=1;i<=12;i++){
+	get_nextval(T,next);
+	for(int i=1;i<=13;i++){
 		printf("%d ",next[i]);
 	}*/
-	
 	return 0;
 }
 
-Status StrAssign(SString &T,char *chars)
+Status StrAssign(SString T,char *chars)
 {
-	//生成一个其值为chars的串T
-	if(strlen(chars) > MAXSTRLEN)
+	//生成一个其值等于chars的串T
+	Status i;
+	if(strlen(T)>MAXSTRLEN)
+	{
 		return ERROR;
+	} 
 	else
 	{
-		T[0] = strlen(chars);
-		for(int i = 1; i <= T[0]; ++i)
+		T[0]=strlen(chars);
+		for(i=1;i<=T[0];i++)
 		{
-			T[i] = chars[i-1];
+			T[i]=*(chars+i-1);
 		}
+		return OK;
 	}
-	return OK;
-}
-
-Status Print(SString T)
+} 
+void StrPrint(SString T)
 {
-	int i;
+	//输出字符串T
+	Status i;
 	printf("%d ",T[0]);
 	for(i=1;i<=T[0];i++)
 	{
 		printf("%c",T[i]);
-	}
+	} 
 	printf("\n");
-	return OK;
 }
 
-int index_KMP(SString S,SString T,int pos)
+int Index_KMP(SString S,SString T,Status pos)
 {
-	int next[100];
-	int i,j;
-    i=pos,j=1;
-    get_next(T,next);
-	while(i<=S[0] && j<=T[0])
+	//利用模式算T的next数组函数求T在主串S中的第pos个字符之后的位置
+	int nextval[100];
+	int i=pos,j=1;
+	get_nextval(T,nextval);
+	while(i<=S[0]&&j<=T[0])
 	{
-		if(j==0 || S[i]==T[j])
+		if(j==0||S[i]==T[j])//继续比较后继字符模式串向右移动 
 		{
 			++i;
 			++j;
 		}
 		else
-			j=next[j];
-	}
+			j=nextval[j]; 
+	} 
 	if(j>T[0])
 		return i-T[0];
 	else
-		return 0;
-}
+		return ERROR;
+} 
 
-void get_next(SString T,int *next)
+void get_nextval(SString T,Status nextval[])//改进next数组函数版 
 {
-	int i=1,j=0;
-	next[1]=0;
+	//求模式串的T的next函数修正值并存入数组nextval中
+	Status i=1,j; 
+	nextval[1]=0;
+	j=0;
 	while(i<T[0])
 	{
 		if(j==0||T[i]==T[j])
 		{
 			++i;
 			++j;
-			next[i]=j;
+			if(T[i]!=T[j])
+				nextval[i]=j;
+			else
+				nextval[i]=nextval[j];
 		}
 		else
-		{
-			j=next[j];
-		}
+			j=nextval[j];
 	}
 }
+
